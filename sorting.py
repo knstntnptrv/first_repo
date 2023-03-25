a6c3da8f62b8ebabfb1bd4d2aba17a183906990c
knstntnptrv <arkadiymogilniy@gmail.com>
2023-03-26 00:28:36 +0300

    генератор словаря из текста v0.1

--- sorting.py
+++ sorting.py
@@ -10,14 +10,20 @@ print(bs.get_text())
 
 
 
-mylist = [x.lower() for x in bs.get_text()]
+mylist = [word.lower().strip() for word in bs.get_text().split() if len(word) > 2]
 print(mylist)
 
-for i in range(len(mylist)):
-    if mylist[i][-1] in ".,-/:;()?!'":
-        mylist[i] = mylist[i][:-1]
 
-mylist_clear = list(set(mylist))
+for i in range(len(mylist)):
+    if mylist[i][-1] in ".,-/:;()?!<>[]{}'":
+        mylist[i] = mylist[i][:-1]
+    if mylist[i].isdigit(): mylist[i] = ''
+
+reslist =[]
+for word in mylist:
+    if word.isalpha(): reslist.append(word)
+mylist_clear = list(set(reslist))
+
 mydict = {}
 for i in mylist_clear:
     count = 0
@@ -25,10 +31,15 @@ for i in mylist_clear:
         if i == j: count += 1
     mydict[i] = count
 
+
 sort_tuple = sorted(mydict.items(), reverse=1, key=lambda item: item[1])
 
-sort_dict = {k: v for k, v in sort_tuple}
-for k, v in mydict.items():
-    print(v, k)
+count = 0
+res_list = [value + '\n' for value, key in sort_tuple]
+with open('/home/arkadiy/py/text.txt', 'a') as f:
+    f.writelines(res_list)
+for k, v in sort_tuple:
+    print(count, v, k)
+    count += 1
 
-print(sort_dict)
+print(res_list)
